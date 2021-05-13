98edda938e4ad60e539b33f8eed313f983425edd
hhkp <s.volodymyr82@gmail.com>
2021-05-13 18:18:48 +0300

    gh-pages

---
+++ precache-manifest.3d4b9b7779432a0fe3116dc68ee79dba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94afbc08b78ddf9e2094",
+    "url": "/fourSchl/css/about.b5e0a679.css"
+  },
+  {
+    "revision": "c9a842fcd443930df1fe",
+    "url": "/fourSchl/css/app.2eac2ac7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/fourSchl/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5da4be4c2b0faa0021ed52566fa49ba3",
+    "url": "/fourSchl/index.html"
+  },
+  {
+    "revision": "94afbc08b78ddf9e2094",
+    "url": "/fourSchl/js/about.8871b0ab.js"
+  },
+  {
+    "revision": "c9a842fcd443930df1fe",
+    "url": "/fourSchl/js/app.bffc72b6.js"
+  },
+  {
+    "revision": "231abebe8aff78c7fc31",
+    "url": "/fourSchl/js/chunk-vendors.529fbf04.js"
+  },
+  {
+    "revision": "9b475f4bfc6c08f1ff35483192922569",
+    "url": "/fourSchl/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/fourSchl/robots.txt"
+  }
+]);
